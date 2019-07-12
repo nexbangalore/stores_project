31dde6a3123791aaffc8a3b664fe6ad671c80d70
rachitapatro <rachitap@nexsemisystems.com>
2019-07-12 18:32:08 +0530

    Open and Read File

--- header.h
+++ header.h
@@ -1,3 +1,13 @@
-void login_existing_user();
-void register_new_user();
-int options_display();
+#include<iostream>
+#include<istream>
+#include<fstream>
+#include<stdlib.h>
+#include<string.h>
+#define IT_FILE "/home/rachita/Documents/project_documents/it_details.txt"
+#define FINANCE_FILE "/home/rachita/Documents/project_documents/finance_details.txt"
+
+#define CHOICE_1 1
+#define CHOICE_2 2
+#define CHOICE_3 3
+
+
