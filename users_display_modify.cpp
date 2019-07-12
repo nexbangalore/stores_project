fb92a016b2921fe6a6e031eac8778f88482f722b
rachitapatro <rachitap@nexsemisystems.com>
2019-07-12 18:29:53 +0530

    Open and Read File

--- users_display_modify.cpp
+++ users_display_modify.cpp
@@ -3,37 +3,42 @@
 #include<fstream>
 #include<stdlib.h>
 #include<string.h>
+#include"header.h"
+#include <string.h>
 
 void choice();
 void display_user_details();
 void modify_user_details();
-void exit();
+
+std::ifstream ifs;
 
 void user_choice(){
-	int choice;
+	
+	int CHOICE;
 	
 	std::cout<<"\n Enter your choice";
 	std::cout<<"\n 1. Display details";
 	std::cout<<"\n 2. Modify details";
 	std::cout<<"\n 3. Exit \n";
 
-	std::cin>>choice;
+	std::cin>>CHOICE;
 
 	// Condition to check the choice input
-	switch(choice){
+	switch(CHOICE){
 
-		case 1:
+		case CHOICE_1:
 		// Fetch the record
 		display_user_details();
 		break;
 
-		case 2:	
+		case CHOICE_2:	
 		// Edit the record
 		modify_user_details();
 		break;
 
-		case 3:
+		case CHOICE_3:
 		// Exit the program
+		system("clear");
 		exit(0);
 		break;
 	
@@ -43,11 +48,39 @@ void user_choice(){
 	}
 };
 
+int read_file(){	
+	std::cout<<"\n File read function: \n";	
+
+	ifs.open ("it_details.txt", std::ifstream::in);
+
+  	char details = ifs.get();
+
+  	while (ifs.good()) {
+    		std::cout << details;
+    		details = ifs.get();
+  	}
+
+  	ifs.close();
+
+	return 0;
+};
+
 void display_user_details(){
+
+	std::string name, department;
+
 	system("clear");	
 	std::cout<<"From display function \n";
-	user_choice();
+	
+	std::cout<<"\n Enter the name of employee:"; 	
+	std::cin>>name;	
+	std::cout<<"\n Enter the department of employee:"; 
+	std::cin>>department;	
 
+	if(department == "IT"){
+		read_file();
+	}
+	user_choice();
 };
 
 void modify_user_details(){
@@ -55,10 +88,6 @@ void modify_user_details(){
 	std::cout<<"From modify function \n";	
 	user_choice();		
 };
-
-void exit(){
-	exit();
-}
 
 int main(){
 
