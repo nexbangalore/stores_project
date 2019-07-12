323419639e1811310b3c49e594228fd7b78a07f4
saikatlaha13 <saikatlaha13@gmail.com>
2019-07-12 17:52:52 +0530

    3rd_time

--- header.h
+++ header.h
@@ -1,3 +1,46 @@
+#pragma once
+
+using namespace std;
+
+#define choice_1 1
+#define choice_2 2
+#define choice_3 3
+
+class emp_id
+{
+    public:
+        int id;
+};
+
+class emp_username
+{
+    public:
+        string username;
+};
+
+class emp_password
+{
+    public:
+        string password;
+};
+
+class emp_department
+{
+    public:
+        string department;
+};
+
+class login:public emp_username,public emp_password,public emp_department
+{
+    
+};
+
+class regstr:public emp_username,public emp_password,public emp_department
+{
+    
+};
+
+   
 void login_existing_user();
 void register_new_user();
 int options_display();
