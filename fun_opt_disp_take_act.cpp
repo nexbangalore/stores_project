75fa7945ede68fa683333782463d95e13a796665
saikatlaha13 <saikatlaha13@gmail.com>
2019-07-13 23:12:55 +0530

    solved the problem of flushing o/p stream

--- fun_opt_disp_take_act.cpp
+++ fun_opt_disp_take_act.cpp
@@ -1,14 +1,15 @@
 #include"header.h"
 
 int options_display()
-{   
-    int option; 
+{
+    int option;   
     cout<<"\n Enter your choice";
 	cout<<"\n 1. Register";
 	cout<<"\n 2. login";
 	cout<<"\n 3. Display details";
 	cout<<"\n 4. Modify details";
 	cout<<"\n 5. Exit \n";
+
     cin>>option;
 
     return option;
@@ -16,30 +17,32 @@ int options_display()
 
 void user_choice()
 {
+    int choice=0;
+    Employee operation;
     system("clear");
-    int CHOICE=options_display();
-    cout<<CHOICE;
 
-    switch(CHOICE)
+    choice=options_display();
+
+    switch(choice)
     {
         case CHOICE_1:
             // register new employee
-            register_new_user();
+            operation.register_new_user(&operation);
             break;
 
         case CHOICE_2:
             // for logging in existing employee
-            login_existing_user();
+            operation.login_existing_user(&operation);
             break;
 
         case CHOICE_3:
             // Fetch the record
-            display_employee_details();
+            operation.display_employee_details(&operation);
             break;
 
         case CHOICE_4:	
             // Edit the record
-            modify_employee_details();
+            operation.modify_employee_details(&operation);
             break;
 
         case CHOICE_5:
@@ -50,60 +53,70 @@ void user_choice()
         default:
             // if the user enter wrong choice    
             cout<<"\n Please enter a correct input \n";
-            user_choice();
+            //user_choice();
     }
+    user_choice();
+
+    return ;
 }
 
-void login_existing_user(){
-
-    login login_object;
+void Employee::display_employee_details(Employee* dips_ptr){
     
     system("clear");
-    cout<<"-------welcome to login page-------"<<"\n";
+    cout<<"welcome to display\n"<<flush;
+    
+    cout<<"\n"<<"enter your username:"<<endl;
+    cin>>(*dips_ptr).username;
+    
+    cout<<"\n"<<"enter your password:"<<endl;
+    cin>>(*dips_ptr).password;
+    
+    return;
+}
+
+void Employee::modify_employee_details(Employee* modify_ptr){
+    
+    system("clear");
+    cout<<"welcome to modify\n"<<flush;
+    
+    cout<<"\n"<<"enter your username:"<<endl;
+    cin>>(*modify_ptr).username;
+    
+    cout<<"\n"<<"enter your password:"<<endl;
+    cin>>(*modify_ptr).password;
+    
+    return;
+}
+
+void Employee::login_existing_user(Employee* login_ptr){
+
+    system("clear");
+    cout<<"-------welcome to login page-------"<<"\n"<<flush;
 
     cout<<"-------enter the credentials-------"<<"\n";
 
     cout<<"\n"<<"enter your username:"<<endl;
-    cin>>login_object.username;
+    cin>>(*login_ptr).username;
 
     cout<<"\n"<<"enter your password:"<<endl;
-    cin>>login_object.password;
+    cin>>(*login_ptr).password;
 
-    user_choice();
+    //user_choice();
 }
 
-void register_new_user()
+void Employee::register_new_user(Employee* regt_ptr)
 {
-    regstr regt_object;
-    
     system("clear");
-    cout<<"-------welcome to registration page------"<<"\n";
+    cout<<"-------welcome to registration page------"<<"\n"<<flush;
 
     cout<<"-------enter the credentials:-------"<<"\n";
     cout<<"\nenter your username:"<<endl;
-    cin>>regt_object.username;
+    cin>>(*regt_ptr).username;
 
     cout<<"\nenter your password:"<<endl;
-    cin>>regt_object.password;
+    cin>>(*regt_ptr).password;
 
-    user_choice();
+    //user_choice();
 }
 
-void display_employee_details(){
-
-    system("clear");	
-    
-    cout<<"From display function \n";
-    
-    user_choice();
-};
-
-void modify_employee_details(){
-    
-    system("clear");	
-
-    cout<<"From modify function \n";	
-    
-    user_choice();		
-};
 
